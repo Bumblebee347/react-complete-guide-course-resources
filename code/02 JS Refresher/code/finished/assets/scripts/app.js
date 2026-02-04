11dfc45d2bdef01ab18447833472fa4dca0d7e6f
tobi <tobias.fleckner@smartindustrycampus.onmicrosoft.com>
2026-02-04 18:25:00 +0100

    added sone testing nothing crutial

--- code/02 JS Refresher/code/finished/assets/scripts/app.js
+++ code/02 JS Refresher/code/finished/assets/scripts/app.js
@@ -1,9 +1,42 @@
-import {apiKey} from "./util.js";
+// import {apiKey} from "./util.js";
+//
+// import defaultImport from "./util.js";
+// console.log(apiKey);
+// console.log(defaultImport);
+//
+// import * as utils from "./util.js";
+// console.log(utils); //js object
+// console.log(utils.apiKey); //js object field
 
-import defaultImport from "./util.js";
-console.log(apiKey);
-console.log(defaultImport);
+// let hobbies = ["Sports", "Reading", "Driving", "Reading"];
+//
+// // console.log(hobbies.findIndex(item => item === "Reading"));
+// // console.log(hobbies.find(item => item.toLowerCase().startsWith("r")));
+// hobbies = hobbies.map(item => item + "!");
+//
+// const objArray = hobbies.map(item => ({name : item}));
+// console.log(objArray);
 
-import * as utils from "./util.js";
-console.log(utils); //js object
-console.log(utils.apiKey); //js object field
+// const {name, age} = {
+//     name: "Hans",
+//     age: 25
+// }
+//
+// console.log(name)
+// console.log(age)
+
+// const item = {
+//     id: 5,
+//     currency: 15.45,
+//     name: "Seife"
+// }
+//
+// function storeItem({id, currency}) {
+//     console.log(id);
+//     console.log(currency);
+// }
+//
+// storeItem(item);
+
+const text = 'Hallo';
+console.log(text.concat(" welt"));
