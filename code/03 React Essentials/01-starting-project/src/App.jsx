8663f2f978bf06386955d1f58ada8fbbf7607f0d
tobi <tobias.fleckner@smartindustrycampus.onmicrosoft.com>
2026-02-09 22:38:01 +0100

    finished first demo project

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,8 +1,33 @@
+import {useState} from "react";
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
-import { CORE_CONCEPTS} from "./data.js";
+import { CORE_CONCEPTS } from "./data.js";
+import TabButton  from "./components/TabButton";
+import { EXAMPLES } from "./data.js";
 
 function App() {
+    const [selected, setSelected] = useState();
+
+    function handleSelect(selectedButton) {
+        console.log(`You clicked the ${selectedButton} button!`);
+        setSelected(selectedButton);
+    }
+
+    let tabContent = <p>Please select a topic.</p>;
+
+    if (selected) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selected].title}</h3>
+                <p>{EXAMPLES[selected].description}</p>
+                <pre>
+                    <code>
+                        {EXAMPLES[selected].code}
+                    </code>
+                </pre>
+            </div>)
+    }
+
     return (
         <div>
             <Header/>
@@ -10,11 +35,18 @@ function App() {
                 <section id = "core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept {...CORE_CONCEPTS[0]}/>
-                        <CoreConcept {...CORE_CONCEPTS[1]}/>
-                        <CoreConcept {...CORE_CONCEPTS[2]}/>
-                        <CoreConcept {...CORE_CONCEPTS[3]}/>
+                        {CORE_CONCEPTS.map((conceptItem)=> <CoreConcept {...conceptItem} key={conceptItem.title} />)}
                     </ul>
+                </section>
+                <section id = "examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton isSelected={selected === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton isSelected={selected === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton isSelected={selected === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton isSelected={selected === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+                    </menu>
+                    {tabContent}
                 </section>
             </main>
         </div>
---
+++ code/03 React Essentials/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,6 @@
+
+export default function ({children, onSelect, isSelected}) {
+    return <li>
+        <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+    </li>
+}
