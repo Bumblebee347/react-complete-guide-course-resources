f914425a2a3f915a4a99543e2c31c8663aaa1276
Tobi [BumbleBee] <tobias.fleckner@gmail.com>
2026-02-16 18:20:03 +0100

    added direct focus and select of the inputfield when clicking edit

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,20 +1,29 @@
-import {useState} from "react";
+import {useState, useEffect, useRef} from "react";
 
 export default function Player({ name: initialName, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
     const [name, setName] = useState(initialName);
+    const inputRef = useRef(null);
+
     function handleEditClick() {
         setIsEditing((editing) => !editing);
     }
 
     let playerName = <span className='player-name'>{name}</span>;
     if (isEditing) {
-        playerName = <input type="text" value={name} onChange={handleChange} required/>
+        playerName = <input ref={inputRef} type="text" value={name} onChange={handleChange} required/>
     }
 
     function handleChange(event){
         setName(event.target.value);
     }
+
+    useEffect(() => {
+        if (isEditing) {
+            inputRef.current.focus();
+            inputRef.current.select();
+        }
+    }, [isEditing]);
 
     return (
         <li>
